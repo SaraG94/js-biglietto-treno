3bf17b398d11fe9755f213a7c50cac7e3533bf8e
SaraG94 <sara.gherbassi.94@gmail.com>
2023-01-31 17:06:02 +0100

    FEAT press name and full priece

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
 //chiedere kw
 let km = prompt ('Salve viaggiatore! Quanti kilometri deve percorre col treno?');
 console.log(km)
+
 //chiedere età
 let age = prompt ('Quanti anni ha il viaggiatore?');
 console.log(age)
+
+//chiedere nome 
+let firstname = prompt ('Qual è il nome e il cognome del viaggiatore?');
+console.log(firstname)
+
 //calcolare prezzo kw
 let fullPrice = (km*0.21);
 console.log(fullPrice)
+
 //applicare sconti
 if (age < 18 ){
     let under18Price = ((km*0.21)*0.8);
@@ -16,5 +23,12 @@ if (age < 18 ){
     console.log(over65Price)
 } 
 
+//prendere elemento html
+let spanElement=document.getElementById('full-price');
+//stampare prezzo intero
+spanElement.innerHTML=fullPrice;
 
-//stampare prezzo finale
+//prendere elementi html 
+let pElement=document.getElementById('passenger');
+//stampare dati viaggiatore
+pElement.innerHTML= firstname + ' ' + age;
